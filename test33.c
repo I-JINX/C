972920f39808ef7b24229acff5c2899798bb4946
I-JINX <1101505164@Qqq.com>
2019-10-18 21:51:31 +0800

    c

---
+++ test33.c
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{//关机。
+	//char input[20] = { 0 };
+	//system("shutdown - s - t 60");
+	//printf("Please note that your computer will be shut down in one minute.\n");
+	//printf("please input 110\n");
+	//scanf("%s", input);
+	//printf(">:\n");
+	//while (1)
+	//{
+	//	if (strcmp(input, "110") == 0)
+	//	{
+	//		system("shutdown -a");
+	//		break;
+	//	}
+	//}
+	//char input[20] = { 0 };//关机goto.
+	//system("shutdown - s - t 60");
+	//again:
+	//printf("Please note that your computer will be shut down in one minute.\n");
+	//printf("please input 110\n");
+	//scanf("%s", input);
+	//printf(">:\n");
+
+	//if (strcmp(input, "110") == 0)
+	//	{
+	//		system("shutdown -a");
+	//		
+	//	}
+	//else 
+	//	{ 
+	//		goto again; 
+	//	}
+	return 0;
+}
