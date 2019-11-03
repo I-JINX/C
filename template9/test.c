11568574d595cd88807872b852c84818242590db
I-JINX <1101505164@Qqq.com>
2019-11-03 18:01:47 +0800

    .h

---
+++ template9/test.c
@@ -0,0 +1,67 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include "game.h"
+void menu()
+{
+	printf("***************************\n");
+	printf("*****    0.exit       *****\n");
+	printf("*****    1.play       *****\n");
+	printf("***************************\n");
+}
+void game()
+{
+	char board[ROW][COL] = { 0 };
+	//初始化棋盘
+	InitBoard(board, ROW, COL);
+	DisplayBoard(board, ROW, COL);
+	char ret = 0;
+	while(1)
+	{
+		PlayerMove(board, ROW, COL);
+		DisplayBoard(board, ROW, COL);
+		ret=IsWin(board, ROW, COL);
+		if (ret != 'C')
+			break;
+		ComputerMove(board,ROW,COL);
+		DisplayBoard(board, ROW, COL);
+		ret = IsWin(board, ROW, COL);
+		if (ret != 'C')
+			break;
+	}if(ret=='*')
+	{
+		printf("玩家赢\n");
+	}else if(ret=='#')
+	{
+		printf("电脑赢\n");
+	}else if(ret=='P')
+	{
+		printf("平局\n");
+	}
+
+}
+void test()
+{
+	int input = 0;
+	srand((unsigned int)time(NULL));
+	do
+	{
+		menu();
+		printf("请选择：");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case 1:
+			game();
+			break;
+		case 0:
+			printf("退出");
+			break;
+		default:
+			break;
+		}
+	} while (input);
+}
+int main()
+{
+	test();
+	return 0;
+}
