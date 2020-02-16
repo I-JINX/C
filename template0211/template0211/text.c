f57039cd622359096c872bd4976af345b2d81eb9
I-JINX <1101505164@Qqq.com>
2020-02-16 10:32:51 +0800

    wenjian

---
+++ template0211/template0211/text.c
@@ -0,0 +1,55 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "Contact.h"
+void menu()
+{
+	printf("*************************  ***\n");
+	printf("***  1.add       2.del     ***\n");
+	printf("***  3.search    4.modify  ***\n");
+	printf("***  5.show      6.sort    ***\n");
+	printf("***  0.exit                ***\n");
+	printf("******************************\n");
+}
+int main()
+{
+	int input = 0;
+
+	//PeoInfo con[1000];
+	//int sz = 0;
+	Contact con;
+
+	InitContact(&con);
+	do
+	{
+		menu();
+		printf("please choose:");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case ADD:
+			ADDContact(&con);
+			break;
+		case DEL:
+			DELContact(&con);
+			break;
+		case SEARCH:
+			break;
+		case MODIFY:
+			break;
+		case SHOW:
+			SHOWContact(&con);
+			break;
+		case SORT:
+			break;
+		case EXIT:
+			SaveContact(&con);
+			DestroyContact(&con);
+			printf("Exit successfully\n");
+			break;
+		default:
+			printf("Selection error\n");
+			break;
+
+		}
+	} while (input);
+	return 0;
+}
